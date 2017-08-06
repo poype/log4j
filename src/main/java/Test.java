f67a21717ec0733cec0bf31d369c1497bef6eb1a
poype <253186926@qq.com>
2017-08-06 23:23:08 +0800

    配置好log4j环境，写了一个Hello World log4j的配置文件是log4j.xml，log4j会自动在classpath下发现这个配置文件

---
+++ src/main/java/Test.java
@@ -0,0 +1,15 @@
+import org.apache.log4j.Logger;
+import org.apache.log4j.spi.LoggerFactory;
+
+/**
+ * Created by poype on 2017/8/6.
+ */
+public class Test {
+
+    private static Logger logger = Logger.getLogger(Test.class);
+
+    public static void main(String[] args) {
+        logger.info("Hello World");
+    }
+
+}
